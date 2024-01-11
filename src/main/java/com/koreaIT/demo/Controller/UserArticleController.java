e9b1955580cdcb941ff76422c40e14c3704ed67c
wdwdwq <qkenr7278@gmail.com>
2024-01-11 21:34:34 +0900

    makeTestData(), writeArticle()

--- src/main/java/com/koreaIT/demo/Controller/UserArticleController.java
+++ src/main/java/com/koreaIT/demo/Controller/UserArticleController.java
@@ -11,33 +11,49 @@ import com.koreaIT.demo.vo.Article;
 
 @Controller
 public class UserArticleController {
-
+	
 	private int lastArticleId;
 	private List<Article> articles;
-
+	
 	public UserArticleController() {
-		this.lastArticleId = 0;
-		this.articles = new ArrayList<>();
-
+		lastArticleId = 0;
+		articles = new ArrayList<>();
+		
+		makeTestData();
 	}
-
-	@RequestMapping("/user/article/doWrite")
-	@ResponseBody
-	public Article doWrite(String title, String body) {
-
-		this.lastArticleId++;
+	
+	// 서비스메서드
+	private void makeTestData() {
+		for (int i = 1; i <= 10; i++) {
+			
+			String title = "제목" + i;
+			String body = "내용" + i;
+			
+			writeArticle(title, body);
+		}
+	}
+	
+	private Article writeArticle(String title, String body) {
+		lastArticleId++;
 		Article article = new Article(lastArticleId, title, body);
-
-		this.articles.add(article);
-
+		articles.add(article);
+		
 		return article;
 	}
 
-	@RequestMapping("/user/article/list")
+	// 액션메서드
+	@RequestMapping("/usr/article/doWrite")
 	@ResponseBody
-	public List<Article> ShowList() {
-
-		return this.articles;
+	public Article doWrite(String title, String body) {
+		Article article = writeArticle(title, body);
+		
+		return article;
 	}
-
+	
+	@RequestMapping("/usr/article/list")
+	@ResponseBody
+	public List<Article> showList() {
+		return articles;
+	}
+	
 }
