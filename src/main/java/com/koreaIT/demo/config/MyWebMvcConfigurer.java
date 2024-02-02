c436271aba98cc027281db7c1234fb948ea38604
wdwdwq <qkenr7278@gmail.com>
2024-02-02 18:36:30 +0900

    interceptor, reply modify delete button

--- src/main/java/com/koreaIT/demo/config/MyWebMvcConfigurer.java
+++ src/main/java/com/koreaIT/demo/config/MyWebMvcConfigurer.java
@@ -1,6 +1,7 @@
 package com.koreaIT.demo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -24,15 +25,29 @@ public class MyWebMvcConfigurer implements WebMvcConfigurer {
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(beforeActionInterceptor).addPathPatterns("/**").excludePathPatterns("/resource/**");
-
-		registry.addInterceptor(needLoginInterceptor).addPathPatterns("/usr/article/write")
-				.addPathPatterns("/usr/article/doWrite").addPathPatterns("/usr/article/modify")
-				.addPathPatterns("/usr/article/doModify").addPathPatterns("/usr/article/doDelete")
-				.addPathPatterns("/usr/member/doLogout");
-
-		registry.addInterceptor(needLogoutInterceptor).addPathPatterns("/usr/member/doJoin")
-				.addPathPatterns("/usr/member/login").addPathPatterns("/usr/member/doLogin");
+		
+		InterceptorRegistration ir;
+		
+		ir = registry.addInterceptor(beforeActionInterceptor);
+		ir.addPathPatterns("/**");
+		ir.addPathPatterns("/favicon.ico");
+		ir.excludePathPatterns("/resource/**");
+		
+		ir = registry.addInterceptor(needLoginInterceptor);
+		ir.addPathPatterns("/usr/article/write");
+		ir.addPathPatterns("/usr/article/doWrite");
+		ir.addPathPatterns("/usr/article/modify");
+		ir.addPathPatterns("/usr/article/doModify");
+		ir.addPathPatterns("/usr/article/doDelete");
+		ir.addPathPatterns("/usr/member/doLogout");
+		ir.addPathPatterns("/usr/recommendPoint/getRecommendPoint");
+		ir.addPathPatterns("/usr/recommendPoint/doRecommendPoint");
+		ir.addPathPatterns("/usr/reply/doWrite");
+		
+		ir = registry.addInterceptor(needLogoutInterceptor);
+		ir.addPathPatterns("/usr/member/doJoin");
+		ir.addPathPatterns("/usr/member/login");
+		ir.addPathPatterns("/usr/member/doLogin");
 	}
 
 }
